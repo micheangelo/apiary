de14320462ebce0240028211c66e32ea0ce29759
micheangelo <narciarz1@interia.pl>
2018-09-25 21:17:45 +0200

    - second commit

--- Apiary/src/main/java/pl/manager/apiary/configuration/RootConfiguration.java
+++ Apiary/src/main/java/pl/manager/apiary/configuration/RootConfiguration.java
@@ -1,10 +1,17 @@
 package pl.manager.apiary.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import pl.manager.apiary.model.Cost;
 
 @Configuration
 @ComponentScan
 public class RootConfiguration {
-
+	
+	@Bean
+	public Cost getNewCost() {
+		return new Cost("test description");
+	}
 }
---
+++ Apiary/src/main/java/pl/manager/apiary/model/Cost.java
@@ -0,0 +1,19 @@
+package pl.manager.apiary.model;
+
+public class Cost {
+	private String description;
+
+	public Cost(String description) {
+		super();
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
