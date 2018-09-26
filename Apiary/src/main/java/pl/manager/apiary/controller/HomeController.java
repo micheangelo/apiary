7c929306227827c9b6b432c70e0ddc7bfb38db03
mzientek <mzientek@kamsoft.pl>
2018-09-26 13:52:29 +0200

    - costs list

--- Apiary/src/main/java/pl/manager/apiary/controller/HomeController.java
+++ Apiary/src/main/java/pl/manager/apiary/controller/HomeController.java
@@ -1,18 +1,14 @@
 package pl.manager.apiary.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import pl.manager.apiary.model.Cost;
+import pl.manager.apiary.service.CostService;
 
 /**
  * Handles requests for the application home page.
@@ -22,13 +18,14 @@ public class HomeController {
 	
 	/*@Autowired
 	private Cost newCost;*/
+	private CostService costService;
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	/*@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 
@@ -40,5 +37,12 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate);
 
 		return "home";
+	}*/
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String listCosts(Model model) {
+		model.addAttribute("cost", new Cost());
+		model.addAttribute("listCosts", this.costService.listCosts());
+		return "cost";
 	}
 }
