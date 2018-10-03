a5c6dbd3d04980da7d3a2f7d52d3b0f7e963656d
micheangelo <narciarz1@interia.pl>
2018-10-03 13:29:49 +0200

    - script

--- Scripts/script.sql
+++ Scripts/script.sql
@@ -1,8 +1,10 @@
 use apiary;
 
-CREATE TABLE cost(
+CREATE TABLE transaction(
  id INT AUTO_INCREMENT primary key NOT NULL,
  description varchar(1000),
- amount int,
- price decimal(15,2)
+ quantity decimal(15,2),
+ price decimal(15,2),
+ transactionType char(1),
+ transactionDate datetime
 );
