0e29f488c3f93b9b0bbc00e9b6b32bda10c5fd95
micheangelo <narciarz1@interia.pl>
2018-10-11 20:01:50 +0200

    Script

--- Scripts/script.sql
+++ Scripts/script.sql
@@ -2,16 +2,35 @@ use apiary;
 /
 CREATE TABLE transaction(
  id INT AUTO_INCREMENT primary key NOT NULL,
- description varchar(1000),
- quantity decimal(15,2),
- price decimal(15,2),
- transactionType char(1),
- transactionDate datetime
+ description VARCHAR(1000),
+ quantity DECIMAL(15,2),
+ price DECIMAL(15,2),
+ transactionType CHAR(1),
+ transactionDate DATETIME
 )
 /
 CREATE TABLE transaction_types (
  id INT AUTO_INCREMENT primary key NOT NULL,
- name varchar(100) not null,
- symbol char(1) not null
+ name VARCHAR(100) not null,
+ symbol CHAR(1) not null
+)
+/
+CREATE TABLE hive (
+ id INT AUTO_INCREMENT primary key NOT NULL,
+ identifier VARCHAR(100) not null,
+ material VARCHAR(200),
+ purchaseYear YEAR,
+ hiveType VARCHAR(100),
+ description VARCHAR(100) 
+)
+/
+CREATE TABLE family (
+ id INT AUTO_INCREMENT primary key NOT NULL,
+ race VARCHAR(100),
+ queenOrigin VARCHAR(100),
+ queenBirthYear YEAR,
+ hiveId INT,
+ FOREIGN KEY (hiveId)
+	REFERENCES hive(id)
 )
 /
